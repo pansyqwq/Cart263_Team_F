d8318ff400ecb628ad61122e5428e345a7c3dbd6
pansyqwq <138616481+pansyqwq@users.noreply.github.com>
2026-04-02 14:32:14 -0400

    added planet and added color

--- CART263-TASK-7/js/TeamF.js
+++ CART263-TASK-7/js/TeamF.js
@@ -10,13 +10,30 @@ export class PlanetF {
 
         //Create planet group
         this.group = new THREE.Group()
-              
+
         // Create planet
         //STEP 1:
         //TODO: Create a planet using THREE.SphereGeometry (Radius must be between 1.5 and 2).
         //TODO: Give it a custom material using THREE.MeshStandardMaterial.
         //TODO: Use castShadow and receiveShadow on the mesh and all future ones so they can cast and receive shadows.
         //TODO: Add the planet mesh to the planet group.
+
+
+        const geometry = new THREE.SphereGeometry(2, 10, 10)//3d dimension of the sphere
+        const material = new THREE.MeshBasicMaterial({ color: 0x95d4ee })
+        const mesh = new THREE.Mesh(geometry, material)
+        this.group.add(mesh)
+
+        // const sizes = {
+        //     width: 800,
+        //     height: 600
+        // }
+        // const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+        // scene.add(camera)
+
+        // camera.position.z = 3
+        // camera.lookAt(mesh.position)
+
 
         //STEP 2: 
         //TODO: Add from 1 to 3 orbiting moons to the planet group. 
@@ -25,22 +42,22 @@ export class PlanetF {
         //STEP 3:
         //TODO: Load Blender models to populate the planet with multiple props and critters by adding them to the planet group.
         //TODO: Make sure to rotate the models so they are oriented correctly relative to the surface of the planet.
-        
+
         //STEP 4:
         //TODO: Use raycasting in the click() method below to detect clicks on the models, and make an animation happen when a model is clicked.
         //TODO: Use your imagination and creativity!
 
         this.scene.add(this.group);
     }
-    
+
     update(delta) {
         // Orbit around sun
         this.angle += this.orbitSpeed * delta * 30;
         this.group.position.x = Math.cos(this.angle) * this.orbitRadius;
         this.group.position.z = Math.sin(this.angle) * this.orbitRadius;
-        
+
         // Rotate planet
-        this.group.rotation.y += delta*5;
+        this.group.rotation.y += delta * 5;
 
         //TODO: Do the moon orbits and the model animations here.
     }
